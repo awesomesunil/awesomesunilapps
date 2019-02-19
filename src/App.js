79221538544e1bb185018941d0f197ddb52b3b8e
Bajracharya <bajracharyas@tkrnt.tfcuintra.net>
2019-02-19 09:19:06 -0600

    some change

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Navbar from './components/layout/Navbar';
-import Index from './components/layout/Index';
-import Lyrics from './components/tracks/Lyrics';
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
+import Image from './components/image/Image'
 
-import './App.css';
+import "./App.css";
 
-import { Provider } from './context';
+import { Provider } from "./context";
 
 class App extends Component {
   render() {
     return (
-      <Provider>
-        <Router>
-          <React.Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Index} />
-                <Route exact path="/lyrics/track/:id" component={Lyrics} />
-              </Switch>
-            </div>
-          </React.Fragment>
-        </Router>
-      </Provider>
+      <div>
+        <img
+          src="https://tfcutulsisd90.tkrnt.tfcuintra.net/InternalDisclosureServer/SaveAbles/jpg"
+          alt=""
+        
+        />
+      </div>
+      // <Provider>
+      //   <Router>
+      //     <React.Fragment>
+      //       <Navbar />
+      //       <div className="container">
+      //         <Switch>
+      //           <Route exact path="/" component={Index} />
+      //           <Route exact path="/lyrics/track/:id" component={Lyrics} />
+      //         </Switch>
+      //       </div>
+      //     </React.Fragment>
+      //   </Router>
+      // </Provider>
     );
   }
 }
---
+++ src/components/image/Image.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Image extends Component {
+  render() {
+    return (
+      <div>
+        <img
+          src="https://tfcutulsisd90.tkrnt.tfcuintra.net/InternalDisclosureServer/SaveAbles/jpg"
+          alt=""
+        />
+      </div>
+    );
+  }
+}
+
+export default Image;
